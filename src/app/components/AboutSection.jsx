f603fb4a04a854ee35df3a50bf73c2926822bd5a
In Tae Chung <intae29@gmail.com>
2025-03-26 17:46:10 -0400

    About Section Tabs/ populated bullet points

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -3,6 +3,52 @@ import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import AboutTabButton from "./AboutTabButton";
 
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2 marker:text-white">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>SQL</li>
+        <li>JavaScript</li>
+        <li>React.js</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2 marker:text-white">
+        <li className="font-bold">
+          Computer Programming - Ontario College Diploma
+        </li>
+        <li className="indent-6 list-none">Seneca College (2024)</li>
+        <li className="font-bold">
+          Honours Law & Society - Bachelor's of Arts
+        </li>
+        <li className="indent-6 list-none">York University (2021)</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2 marker:text-white">
+        <li className="font-bold">Human Rights 101</li>
+        <li className="indent-6 list-none">Issued: The Ontario Rights Commission (Nov 27, 2024)</li>
+        <li className="font-bold">
+          Integrated Accessibility Standards Regulations Training
+        </li>
+        <li className="indent-6 list-none">Issued: AccessForward (Nov 27, 2024)</li>
+      </ul>
+    ),
+  },
+];
+
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
   const [isPending, startTransition] = useTransition();
@@ -23,7 +69,7 @@ const AboutSection = () => {
           width={500}
           height={500}
         />
-        <div>
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
             I enjoy being challenged while learning to code in various languages
@@ -33,7 +79,7 @@ const AboutSection = () => {
             hobbies include film photography, playing the guitar, and finding
             delicious food.
           </p>
-          <div className="flex flex-row mt-8">
+          <div className="flex flex-row justify-start mt-8">
             <AboutTabButton
               selectTab={() => handleTabChange("skills")}
               active={tab === "skills"}
@@ -47,12 +93,14 @@ const AboutSection = () => {
               Education
             </AboutTabButton>
             <AboutTabButton
-              selectTab={() => handleTabChange("certification")}
-              active={tab === "certification"}
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
             >
-              Certification
+              Certifications
             </AboutTabButton>
-            
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
           </div>
         </div>
       </div>
